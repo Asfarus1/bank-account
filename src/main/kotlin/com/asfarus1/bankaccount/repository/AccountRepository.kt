cc9a2982de0535f9bd85ec119a35a13ba3c1ea62
Asfarus1 <m.a.v.norm@mail.ru>
2020-10-22 19:11:21 +0300

    fix lock level and curl in README.MD

--- src/main/kotlin/com/asfarus1/bankaccount/repository/AccountRepository.kt
+++ src/main/kotlin/com/asfarus1/bankaccount/repository/AccountRepository.kt
@@ -9,7 +9,7 @@ import javax.persistence.LockModeType
 
 interface AccountRepository : JpaRepository<Account, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT a from Account a where a.id = :id")
     fun findForUpdate(id: Long): Optional<Account>
 }
